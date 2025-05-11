f961c28c2d1b364040199cc0eccfca85866defc5
hezz <liuxiaohan@nucleisys.com>
2025-05-11 22:41:19 +0800

    add mutex

--- FreeRTOS/include/FreeRTOSConfig.h
+++ FreeRTOS/include/FreeRTOSConfig.h
@@ -84,5 +84,8 @@ configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
 NVIC value of 255. */
 #define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
 
+
+#define configUSE_MUTEXES 1  // 启用互斥量功能
+
 #endif /* FREERTOS_CONFIG_H */
 
--- User/main.c
+++ User/main.c
@@ -2,42 +2,72 @@
 #include <stdio.h>
 #include "freertos.h"
 #include "task.h"
+#include "semphr.h"
 /* -------------------------------- user define header -------------------------------- */
 #include "main.h"
 
 volatile unsigned char mode = 1;    // 切换程序标志位
 
-TaskHandle_t mytestHandler;
+TaskHandle_t key_control_handler;
+TaskHandle_t mode_switch_handler;
 
-void test(void * arg)
+// 定义互斥锁句柄
+SemaphoreHandle_t mode_mutex;
+
+void key_control(void *arg)
 {
-	while (1) {
-		if(1 == Key_GetNum()) {
+	while(1) {
+		int key_state = Key_GetNum();
+		if(key_state == 1) {
+			xSemaphoreTake(mode_mutex, portMAX_DELAY);
 			mode ++;
 			if(mode > 3) {
 				mode = 1;
 			}
-			Digital_Display(mode);	// LED显示当前模式
+			xSemaphoreGive(mode_mutex);
+
+			vTaskDelay(pdMS_TO_TICKS(200)); // 增加按键去抖动延迟
+			printf("key_control - Mode changed to %d\r\n", mode);
 		} else {
-			switch(mode) {
-				case 1: 
-					Robot_Traction();
-					EXTI_DeInit();
-				break;
-
-				case 2:
-					Robot_Avoidance();
-				break;
-
-				case 3:
-					Irscan();
-				break;
-
-				default:
-					mode = 1;
-				break;
-			}
+			printf("key_control - No key press detected\r\n");
 		}
+
+		Digital_Display(mode);	// LED display current mode
+		vTaskDelay(pdMS_TO_TICKS(50));
+		printf("key_control - Delayed\r\n");
+	}
+}
+
+void mode_switch(void * arg)
+{
+	while (1) {
+		xSemaphoreTake(mode_mutex, portMAX_DELAY);
+		switch(mode) {
+			case 1: 
+				Robot_Traction();
+				EXTI_DeInit();
+				printf("mode_switch - Case 1\r\n");
+			break;
+
+			case 2:
+				Robot_Avoidance();
+				printf("mode_switch - Case 2\r\n");
+			break;
+
+			case 3:
+				Irscan();
+				printf("mode_switch - Case 3\r\n");
+			break;
+
+			default:
+				mode = 1;
+				printf("mode_switch - Default case\r\n");
+			break;
+		}
+		xSemaphoreGive(mode_mutex);
+
+		vTaskDelay(pdMS_TO_TICKS(20));
+		printf("mode_switch - Delayed\r\n");
 	}
 }
 
@@ -51,6 +81,8 @@ static void DeviceInit(void)
 	LEDSEG_Init();			   // LED初始化
 	Serial_Init();             // 串口初始化
 	robot_Init();              // 机器人初始化
+
+	printf("Device initialization completed.\r\n");
 }
 
 static void PrintfInfo(void)
@@ -70,11 +102,23 @@ int main(void)
 	DeviceInit();
 	/* print some hard info */
 	PrintfInfo();
-	
+
+	// 创建互斥锁
+	mode_mutex = xSemaphoreCreateMutex();
+
+	if (mode_mutex == NULL) {
+		printf("Failed to create mutex.\r\n");
+		while(1);
+	}
+
 	/* 创建进程 */
-	xTaskCreate(test, "freetest", 512,NULL,2,&mytestHandler);
+	xTaskCreate(key_control, "key_control", 512,NULL,1,&key_control_handler);
+	xTaskCreate(mode_switch, "mode_switch", 512,NULL,2,&mode_switch_handler);
 
 	vTaskStartScheduler();
 	
 	while(1){} /* 正常情况代码不会跑到这里 */
 }
+
+
+
